827dc5f4736e04c9e00616bcdb1a390f0c4474d0
EvgChe <chetkova@list.ru>
2018-02-25 16:01:50 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    var N = str.length / 2;
+
+    for (var n = 0; n < N; n++) {
+
+        for (var i = 0; i < bracketsConfig.length; i++) {
+
+            var bracketType = bracketsConfig[i][0] + bracketsConfig[i][1];
+
+            while (str.indexOf(bracketType) >= 0) {
+
+                str = str.replace(bracketType, '');
+
+            }
+        }
+    }
+    if (str == '') {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 }
